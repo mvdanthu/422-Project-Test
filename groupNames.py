9e4e631ffbf8fcf33a88d4744a7001753971b6ba
amforeman <116131535+amforeman@users.noreply.github.com>
2023-03-08 15:46:10 -0600

    Update groupNames.py

--- groupNames.py
+++ groupNames.py
@@ -1,4 +1,4 @@
-name = ["Dez", "Mo"] #add your name here
+name = ["Dez", "Mo", "Annie"] #add your name here
 print("The following group members have edited the git Repo: ")
 for x in name:
     print(x, end= ". ")
